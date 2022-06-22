b60efb61a75e070fb978df6e207ff38c6cf46975
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-06-22 18:49:31 +0200

    Added Recipient doctor (to field) to Mail class, with unit tests

--- dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/domain/model/Mail.java
+++ dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/domain/model/Mail.java
@@ -1,11 +1,17 @@
 package fr.cnam.stefangeorgesco.dmp.domain.model;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public class Mail extends PatientFileItem {
-	
+
 	@NotBlank(message = "mail text is mandatory")
 	String text;
+
+	@NotNull(message = "recipient doctor is mandatory")
+	@Valid
+	private Doctor to;
 
 	public String getText() {
 		return text;
@@ -13,6 +19,14 @@ public class Mail extends PatientFileItem {
 
 	public void setText(String text) {
 		this.text = text;
+	}
+
+	public Doctor getTo() {
+		return to;
+	}
+
+	public void setTo(Doctor to) {
+		this.to = to;
 	}
 
 }
