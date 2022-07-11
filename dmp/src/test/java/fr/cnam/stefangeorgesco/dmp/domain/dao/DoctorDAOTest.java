edca5612afda53ce0e8f9b52aaf9f3e863edcea2
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-07-11 20:04:58 +0200

    Reorganize FileDAO tests and add DoctorDAO.save() tests

---
+++ dmp/src/test/java/fr/cnam/stefangeorgesco/dmp/domain/dao/DoctorDAOTest.java
@@ -0,0 +1,101 @@
+package fr.cnam.stefangeorgesco.dmp.domain.dao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+
+import fr.cnam.stefangeorgesco.dmp.domain.model.Address;
+import fr.cnam.stefangeorgesco.dmp.domain.model.Doctor;
+import fr.cnam.stefangeorgesco.dmp.domain.model.Specialty;
+
+@TestPropertySource("/application-test.properties")
+@SpringBootTest
+@SqlGroup({
+    @Sql(scripts = "/sql/create-files.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(scripts = "/sql/delete-files.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+public class DoctorDAOTest {
+	
+	@Autowired
+	SpecialtyDAO specialtyDAO;
+
+	@Autowired
+	DoctorDAO doctorDAO;
+	
+	@Autowired
+	private Address doctorAddress;
+	
+	@Autowired
+	private Specialty specialty;
+
+	@Autowired
+	private Doctor doctor;
+	
+	private List<Specialty> specialties;
+
+	@Test
+	public void testDoctorDAOExistsById() {
+		assertTrue(doctorDAO.existsById("1"));
+		assertFalse(doctorDAO.existsById("0"));
+		assertFalse(doctorDAO.existsById("2"));
+	}
+	
+	@Test
+	public void testDoctorDAOFindById() {
+		
+		Optional<Doctor> optionalDoctor = doctorDAO.findById("1");
+		
+		assertTrue(optionalDoctor.isPresent());
+		
+		Doctor doctor = optionalDoctor.get();
+		
+		assertEquals(doctor.getFirstname(), "John");
+		assertEquals(doctor.getLastname(), "Smith");
+		
+	}
+	
+	@Test
+	public void testDoctorDAOSave() {
+		
+		assertFalse(doctorDAO.existsById("doctorId"));
+		
+		specialty.setId("s001");
+		specialty.setDescription("A specialty");
+
+		specialtyDAO.save(specialty);
+
+		specialties = new ArrayList<>();
+		specialties.add(specialty);
+
+		doctorAddress.setStreet1("street");
+		doctorAddress.setCity("city");
+		doctorAddress.setZipcode("zip");
+		doctorAddress.setCountry("country");
+
+		doctor.setId("doctorId");
+		doctor.setFirstname("firstname");
+		doctor.setLastname("lastname");
+		doctor.setPhone("0123456789");
+		doctor.setEmail("doctor@doctors.com");
+		doctor.setAddress(doctorAddress);
+		doctor.setSpecialties(specialties);
+		doctor.setSecurityCode("12345678");
+		
+		doctorDAO.save(doctor);
+		
+		assertTrue(doctorDAO.existsById("doctorId"));
+		
+	}
+	
+}
--- dmp/src/test/java/fr/cnam/stefangeorgesco/dmp/domain/dao/PatientFileDAOTest.java
+++ dmp/src/test/java/fr/cnam/stefangeorgesco/dmp/domain/dao/PatientFileDAOTest.java
@@ -7,14 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 
-import fr.cnam.stefangeorgesco.dmp.domain.model.File;
+import fr.cnam.stefangeorgesco.dmp.domain.model.PatientFile;
 
 @TestPropertySource("/application-test.properties")
 @SpringBootTest
@@ -22,29 +21,29 @@ import fr.cnam.stefangeorgesco.dmp.domain.model.File;
     @Sql(scripts = "/sql/create-files.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
     @Sql(scripts = "/sql/delete-files.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 })
-public class FileDAOTest {
+public class PatientFileDAOTest {
 	
 	@Autowired
-	private FileDAO fileDAO;
+	private PatientFileDAO patientFileDAO;
 	
 	@Test
-	public void testFileDAOExistsById() {
-		assertTrue(fileDAO.existsById("1"));
-		assertTrue(fileDAO.existsById("2"));
-		assertFalse(fileDAO.existsById("0"));
+	public void testPatientFileDAOExistsById() {
+		assertFalse(patientFileDAO.existsById("1"));
+		assertFalse(patientFileDAO.existsById("0"));
+		assertTrue(patientFileDAO.existsById("2"));
 	}
 	
 	@Test
-	public void testFileDAOFindById() {
+	public void testPatientFileDAOFindById() {
 		
-		Optional<File> optionalFile = fileDAO.findById("1");
+		Optional<PatientFile> optionalPatientFile = patientFileDAO.findById("2");
 		
-		assertTrue(optionalFile.isPresent());
+		assertTrue(optionalPatientFile.isPresent());
 		
-		File file = optionalFile.get();
+		PatientFile patientFile = optionalPatientFile.get();
 		
-		assertEquals(file.getFirstname(), "John");
-		assertEquals(file.getLastname(), "Smith");
+		assertEquals(patientFile.getFirstname(), "Eric");
+		assertEquals(patientFile.getLastname(), "Martin");
 		
 	}
 	
