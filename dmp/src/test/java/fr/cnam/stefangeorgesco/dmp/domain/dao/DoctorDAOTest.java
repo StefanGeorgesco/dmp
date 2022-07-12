c4dccc3dfc8c74e183913fc7a7f4f6947ae1f91c
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-07-12 16:07:16 +0200

    DoctorDAO.delete() unit test

--- dmp/src/test/java/fr/cnam/stefangeorgesco/dmp/domain/dao/DoctorDAOTest.java
+++ dmp/src/test/java/fr/cnam/stefangeorgesco/dmp/domain/dao/DoctorDAOTest.java
@@ -1,5 +1,6 @@
 package fr.cnam.stefangeorgesco.dmp.domain.dao;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -7,12 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -23,29 +26,30 @@ import fr.cnam.stefangeorgesco.dmp.domain.model.Specialty;
 
 @TestPropertySource("/application-test.properties")
 @SpringBootTest
-@SqlGroup({
-    @Sql(scripts = "/sql/create-files.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
-    @Sql(scripts = "/sql/delete-files.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
-})
+@SqlGroup({ @Sql(scripts = "/sql/create-files.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+		@Sql(scripts = "/sql/delete-files.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD) })
 public class DoctorDAOTest {
-	
+
+	@Autowired
+	private JdbcTemplate jdbc;
+
 	@Autowired
 	SpecialtyDAO specialtyDAO;
 
 	@Autowired
 	DoctorDAO doctorDAO;
-	
+
 	@Autowired
 	private Address doctorAddress;
-	
+
 	@Autowired
 	private Specialty specialty;
 
 	@Autowired
 	private Doctor doctor;
-	
+
 	private List<Specialty> specialties;
-	
+
 	@BeforeEach
 	public void setup() {
 		specialty.setId("s001");
@@ -73,34 +77,62 @@ public class DoctorDAOTest {
 		assertFalse(doctorDAO.existsById("0"));
 		assertFalse(doctorDAO.existsById("2"));
 	}
-	
+
 	@Test
 	public void testDoctorDAOFindById() {
 		Optional<Doctor> optionalDoctor = doctorDAO.findById("1");
-		
+
 		assertTrue(optionalDoctor.isPresent());
-		
+
 		Doctor doctor = optionalDoctor.get();
-		
+
 		assertEquals(doctor.getFirstname(), "John");
 		assertEquals(doctor.getLastname(), "Smith");
 	}
-	
+
 	@Test
 	public void testDoctorDAOSave() {
 		assertFalse(doctorDAO.existsById("doctorId"));
-				
+
 		doctorDAO.save(doctor);
-		
+
 		assertTrue(doctorDAO.existsById("doctorId"));
 	}
-	
+
 	@Test
 	public void testDoctorDAOSaveFailureInvalidData() {
 		doctor.getSpecialties().clear();
-		
+
 		assertThrows(RuntimeException.class, () -> doctorDAO.save(doctor));
 		assertFalse(doctorDAO.existsById("doctorId"));
 	}
+
+	@Test
+	void testDoctorDAODeleteSuccess() {
+		assertTrue(doctorDAO.existsById("3"));
+		List<Map<String, Object>> results = jdbc.queryForList(
+						"SELECT * FROM t_doctor_specialty where t_doctor_specialty.doctor_id='3';");
+		assertEquals(2, results.size());
+
+		doctor.setId("3");
+
+		assertDoesNotThrow(() -> doctorDAO.delete(doctor));
+
+		assertFalse(doctorDAO.existsById("3"));
+		results = jdbc.queryForList(
+				"SELECT * FROM t_doctor_specialty where t_doctor_specialty.doctor_id='3';");
+		assertEquals(0, results.size());
+	}
 	
+	@Test
+	public void testDoctorDAODeleteFailureDoctorReferringDoctor() {
+		assertTrue(doctorDAO.existsById("1"));
+		
+		doctor.setId("1");
+		
+		assertThrows(RuntimeException.class, () -> doctorDAO.delete(doctor));
+		
+		assertTrue(doctorDAO.existsById("1"));
+	}
+
 }
