6ae9f52ee6a44c193156cf90483134986efd0e28
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-08-03 21:17:16 +0200

    Jackson inheritance annotations on PatientFileItemDTO

--- dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/domain/dto/PatientFileItemDTO.java
+++ dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/domain/dto/PatientFileItemDTO.java
@@ -7,11 +7,22 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
+@JsonSubTypes({ @JsonSubTypes.Type(value = ActDTO.class, name = "act"),
+		@JsonSubTypes.Type(value = DiagnosisDTO.class, name = "diagnosis"),
+		@JsonSubTypes.Type(value = MailDTO.class, name = "mail"),
+		@JsonSubTypes.Type(value = PrescriptionDTO.class, name = "prescription"),
+		@JsonSubTypes.Type(value = SymptomDTO.class, name = "symptom") })
 public abstract class PatientFileItemDTO {
 
 	private long id;
@@ -19,18 +30,18 @@ public abstract class PatientFileItemDTO {
 	@NotNull(message = "patient file date is mandatory")
 	@PastOrPresent(message = "patient file item date must be in the past or today")
 	private LocalDate date;
-	
+
 	private String comments;
-	
+
 	@NotBlank(message = "authoring doctor id is mandatory")
 	private String authoringDoctorId;
-	
+
 	private String authoringDoctorFirstname;
-	
+
 	private String authoringDoctorLastname;
-	
+
 	private List<String> authoringDoctorSpecialties;
-	
+
 	@NotBlank(message = "patient file id is mandatory")
 	private String patientFileId;
 
