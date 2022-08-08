9e4f000d2c698947668cee3ff7c8e18d2c74eea7
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-08-08 20:39:55 +0200

    DoctorServiceTest add test case delete doctor does not exist

--- dmp/src/test/java/fr/cnam/stefangeorgesco/dmp/domain/service/DoctorServiceTest.java
+++ dmp/src/test/java/fr/cnam/stefangeorgesco/dmp/domain/service/DoctorServiceTest.java
@@ -315,7 +315,7 @@ public class DoctorServiceTest {
 	@Test
 	public void testDeleteDoctorSuccessNoUser() throws DeleteException {
 		doNothing().when(doctorDAO).deleteById("D002");
-		doThrow(new DeleteException("doctor could not be deleted")).when(userService).deleteUser("D002");
+		doThrow(new DeleteException("user could not be deleted")).when(userService).deleteUser("D002");
 		
 		assertDoesNotThrow(() -> doctorService.deleteDoctor("D002"));
 		
@@ -334,6 +334,18 @@ public class DoctorServiceTest {
 		verify(userService, times(1)).deleteUser("D001");
 	}
 	
+	@Test
+	public void testDeleteDoctorFailureDoctorDoesNotExist() throws DeleteException {
+		doThrow(new RuntimeException("...")).when(doctorDAO).deleteById("D003");
+		doThrow(new DeleteException("user could not be deleted")).when(userService).deleteUser("D003");
+		
+		DeleteException ex = assertThrows(DeleteException.class, () -> doctorService.deleteDoctor("D003"));
+		
+		verify(doctorDAO, times(1)).deleteById("D003");
+		verify(userService, times(0)).deleteUser("D003");
+		assertEquals("doctor could not be deleted: ...", ex.getMessage());
+	}
+
 	@Test
 	public void testFindDoctorsByIdOrFirstnameOrLastnameFound2() {
 		when(doctorDAO.findByIdOrFirstnameOrLastname("la")).thenReturn(List.of(foundDoctor1, foundDoctor2));
