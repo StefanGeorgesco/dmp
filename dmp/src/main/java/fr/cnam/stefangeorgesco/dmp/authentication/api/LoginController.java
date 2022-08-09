742e261d843c2373e8204ffb8f1095907b8e11e5
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-08-09 07:59:39 +0200

    LoginController uses UserService, not UserDAO

--- dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/authentication/api/LoginController.java
+++ dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/authentication/api/LoginController.java
@@ -7,15 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.cnam.stefangeorgesco.dmp.authentication.domain.dao.UserDAO;
 import fr.cnam.stefangeorgesco.dmp.authentication.domain.dto.UserDTO;
+import fr.cnam.stefangeorgesco.dmp.authentication.domain.service.UserService;
 import fr.cnam.stefangeorgesco.dmp.exception.domain.FinderException;
 
 @RestController
 public class LoginController {
 
 	@Autowired
-	UserDAO userDAO;
+	UserService userService;
 
 	@Autowired
 	ModelMapper userModelMapper;
@@ -23,7 +23,7 @@ public class LoginController {
 	@PostMapping("/login")
 	public ResponseEntity<UserDTO> login(Principal principal) throws FinderException {
 		
-		UserDTO userDTO = userModelMapper.map(userDAO.findByUsername(principal.getName()).get(), UserDTO.class);
+		UserDTO userDTO = userModelMapper.map(userService.findUserByUsername(principal.getName()), UserDTO.class);
 
 		return ResponseEntity.ok(userDTO);
 
