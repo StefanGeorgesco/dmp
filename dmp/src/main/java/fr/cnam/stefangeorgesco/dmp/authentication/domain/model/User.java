78eebb21bbcc54eef71f463e12ab0b869cdc37f8
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-08-28 16:17:38 +0200

    User securityCode attribute made transient

--- dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/authentication/domain/model/User.java
+++ dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/authentication/domain/model/User.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
@@ -11,6 +12,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Objet du modèle d'authentification représentant l'utilisateur.
+ * 
+ * @author Stéfan Georgesco
+ *
+ */
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,10 +25,17 @@ import lombok.Setter;
 @Table(name = "t_user")
 public class User implements IUser {
 
+	/**
+	 * L'identifiant est identique à celui du dossier de médecin ou du dossier patient
+	 * auquel l'utilisateur est associé (propriétaire du dossier) et permet cette association.
+	 */
 	@Id
 	@NotBlank(message = "L'identifiant est obligatoire.")
 	private String id;
 	
+	/**
+	 * Nom d'utilisateur pour l'identification et l'authentification de l'utilisateur.
+	 */
 	@Column(unique=true)
 	@NotBlank(message = "Le non utilisateur est obligatoire.")
 	private String username;
@@ -33,7 +47,13 @@ public class User implements IUser {
 	@Size(min=4, message = "Le mot de passe doit contenir au moins 4 caractères.")
 	private String password;
 	
-	@Column(name = "security_code", nullable = false)
+	/**
+	 * Ce code généré lors de la création du dossier de médecin ou du dossier patient
+	 * auquel l'utilisateur veut s'associer permet d'authentifier l'utilisateur lors de
+	 * la création de son compte utilisateur et de valider cette association.
+	 * Il n'est pas conservé dans la persistance.
+	 */
+	@Transient
 	private String securityCode;
 	
 }
--- dmp/src/main/resources/db/migration/V1__add_users.sql
+++ dmp/src/main/resources/db/migration/V1__add_users.sql
@@ -5,12 +5,11 @@ CREATE TABLE `t_user` (
   `username` VARCHAR(255) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `role` VARCHAR(255) NOT NULL,
-  `security_code` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK_username_user` (`username`)
 ) ENGINE=InnoDB;
 
-INSERT INTO `t_user` VALUES ('A001','admin','$2a$12$yjbMztwlMm0yEIVbM8ybu.nJ6kQPipI6ViV/8GbU8TWgIwSvRbAQa','ROLE_ADMIN','$2a$10$nq3Ep1OrjUcrYy824CiaNeKgRIIjaQMpz5E5MJWgytFWbJsjpgozu');
-INSERT INTO `t_user` VALUES ('D001','user','$2a$12$7/n1myGRPalYXRCUbsrXz.vhtJCYTfi8j1dBlX1m/ECFosXD6jcMa','ROLE_DOCTOR','$2a$10$nq3Ep1OrjUcrYy824CiaNeKgRIIjaQMpz5E5MJWgytFWbJsjpgozu');
-INSERT INTO `t_user` VALUES ('D002','doc','$2a$12$rbh2MVQ3zMmeE4a.vHQcJOlBQ7uLLE9fpcy3G.l.vfT96WhlF/51m','ROLE_DOCTOR','$2a$10$nq3Ep1OrjUcrYy824CiaNeKgRIIjaQMpz5E5MJWgytFWbJsjpgozu');
-INSERT INTO `t_user` VALUES ('P001','jean','$2a$12$nGajrCywpBYm9xLtu.lAbuTTNCFX3rrHaisz87P.fw2BXCF8E/gD2','ROLE_PATIENT','$2a$10$nq3Ep1OrjUcrYy824CiaNeKgRIIjaQMpz5E5MJWgytFWbJsjpgozu');
+INSERT INTO `t_user` VALUES ('A001','admin','$2a$12$yjbMztwlMm0yEIVbM8ybu.nJ6kQPipI6ViV/8GbU8TWgIwSvRbAQa','ROLE_ADMIN');
+INSERT INTO `t_user` VALUES ('D001','user','$2a$12$7/n1myGRPalYXRCUbsrXz.vhtJCYTfi8j1dBlX1m/ECFosXD6jcMa','ROLE_DOCTOR');
+INSERT INTO `t_user` VALUES ('D002','doc','$2a$12$rbh2MVQ3zMmeE4a.vHQcJOlBQ7uLLE9fpcy3G.l.vfT96WhlF/51m','ROLE_DOCTOR');
+INSERT INTO `t_user` VALUES ('P001','jean','$2a$12$nGajrCywpBYm9xLtu.lAbuTTNCFX3rrHaisz87P.fw2BXCF8E/gD2','ROLE_PATIENT');
