00a7d516deeea80e1fd36f7aba6469f117a62c16
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-08-30 15:28:08 +0200

    Spring initializr version

---
+++ rnipp/src/main/java/fr/cnam/stefangeorgesco/rnipp/RnippApplication.java
@@ -0,0 +1,13 @@
+package fr.cnam.stefangeorgesco.rnipp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnippApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnippApplication.class, args);
+	}
+
+}
