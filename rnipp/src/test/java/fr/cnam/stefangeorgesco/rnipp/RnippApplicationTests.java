794972f2a85bf28b3ef643b80b7453a23df61260
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-08-30 15:40:04 +0200

    Spring intializr version, > java 15

---
+++ rnipp/src/test/java/fr/cnam/stefangeorgesco/rnipp/RnippApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cnam.stefangeorgesco.rnipp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnippApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
