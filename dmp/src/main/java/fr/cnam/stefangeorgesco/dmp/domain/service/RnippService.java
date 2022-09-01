1edc3277e945361135f260b97607bc9fa0803bfc
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-09-01 20:40:28 +0200

    Rewrote RnippService & added timeout to WebClient

--- dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/domain/service/RnippService.java
+++ dmp/src/main/java/fr/cnam/stefangeorgesco/dmp/domain/service/RnippService.java
@@ -1,5 +1,8 @@
 package fr.cnam.stefangeorgesco.dmp.domain.service;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,21 +37,35 @@ public class RnippService {
 	 * @throws CheckException les données du dossier patient ne correspondent pas à
 	 *                        un enregistrement du RNIPP.
 	 */
+	@SuppressWarnings("unchecked")
 	public void checkPatientData(@Valid PatientFileDTO patientFileDTO) throws CheckException {
 
 		RnippRecord record = new RnippRecord(patientFileDTO.getId(), patientFileDTO.getFirstname(),
 				patientFileDTO.getLastname(), patientFileDTO.getDateOfBirth());
 
-		RnippResponse response = rnippClient.post().uri("/check").bodyValue(record).retrieve()
-				.onStatus(HttpStatus::is5xxServerError,
-						clientResponse -> Mono.error(new CheckException("Impossible de joindre le service RNIPP.")))
-				.onStatus(HttpStatus::is4xxClientError,
-						clientResponse -> Mono.error(new CheckException("Requête RNIPP incorrecte.")))
-				.bodyToMono(RnippResponse.class).block();
+		Mono<Object> responseObject = rnippClient.post().uri("/check").bodyValue(record).exchangeToMono(resp -> {
+			if (resp.statusCode().equals(HttpStatus.OK)) {
+				return resp.bodyToMono(RnippResponse.class);
+			} else if (resp.statusCode().equals(HttpStatus.NOT_ACCEPTABLE)) {
+				return resp.bodyToMono(Map.class);
+			} else {
+				return resp.createException().flatMap(Mono::error);
+			}
+		});
 
-		if (!response.getResult()) {
-			throw new CheckException("Les données fournies sont incorrectes. Pas d'enregistrement RNIPP.");
+		Object response = responseObject.block();
+
+		if (response instanceof RnippResponse) {
+			if (!((RnippResponse) response).getResult()) {
+				throw new CheckException("Les données fournies sont incorrectes. Pas d'enregistrement RNIPP.");
+			}
+		} else if (response instanceof Map) {
+			throw new CheckException("RNIPP - " +
+					((Map<String, String>) response).entrySet().stream()
+					.map(entry -> entry.getKey() + " : " + entry.getValue())
+					.collect(Collectors.joining(", ")));
 		}
+
 	}
 
 }
